e13e60284f6ad1c9a5d3bebea3520ac136347974
Haley <hschafer@umich.edu>
2019-02-14 12:31:06 +0000

    03 adding/writing to database code review and bug fix

--- 03-AddingATask-WritingToTheDatabase/01-submit_add_task_form_data/app.py
+++ 03-AddingATask-WritingToTheDatabase/01-submit_add_task_form_data/app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb://admin:admin@ds123926.mlab.com:23926/task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
@@ -13,15 +13,15 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", 
-        tasks=mongo.db.tasks.find())
-    
-    
+                           tasks=mongo.db.tasks.find())
+
+
 @app.route('/add_task')
 def add_task():
     return render_template('addtask.html',
-        categories=mongo.db.categories.find())
-    
-    
+                           categories=mongo.db.categories.find())
+
+
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
     tasks = mongo.db.tasks
@@ -31,7 +31,5 @@ def insert_task():
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
-
-    
+            port=int(os.environ.get('PORT')),
+            debug=True)
